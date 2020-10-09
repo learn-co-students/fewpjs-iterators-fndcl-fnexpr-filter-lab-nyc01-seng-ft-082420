a32fbf9bd2111c8ea81dd19d3f1b378a696cf911
Eddy_Yunhao_Gao <as510891328@gmail.com>
2020-10-08 22:31:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string){
+  return drivers.filter( n => n.toLowerCase() === string.toLowerCase() )
+}
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(n => n.toLowerCase().indexOf(string.toLowerCase()) === 0)
+}
+
+function matchName(source, soughtName) {
+  return source.filter( record => record.name === soughtName)
+}
